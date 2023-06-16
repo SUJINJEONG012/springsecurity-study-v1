1d1260cf1341a2518c8f56b92bee446eb5ab336e
angela <peekaboo32@naver.com>
2023-06-16 15:00:05 +0900

    스프링부트 시큐리티 개념공부하기 버전1

---
+++ src/main/java/com/example/demo/SpringSecurityStudyV1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityStudyV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityStudyV1Application.class, args);
+	}
+
+}
