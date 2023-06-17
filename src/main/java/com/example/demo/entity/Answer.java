709344f6299caeb66732d9892d9fefcb3b3622f4
SUJINJEONG012 <peekaboo32@naver.com>
2023-06-18 04:24:21 +0900

    answer  생성후, 연관관계 설정 manytoone, onetomany  , 디비에서는 foreignkey로 설정된다.

--- src/main/java/com/example/demo/entity/Answer.java
+++ src/main/java/com/example/demo/entity/Answer.java
@@ -1,34 +1,38 @@
 package com.example.demo.entity;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 import org.springframework.data.annotation.Id;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
 
 @Data
 @Entity
-public class Question {
-	
+public class Answer {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
-	@Column(length=200)
-	private String subject;
 	@Column(columnDefinition = "TEXT")
 	private String content;
 	
 	private LocalDateTime createDate;
 	
-	@OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
-	private List<Answer> answerList;
+	@ManyToOne
+	private Question question;
+	/*
+	 * 답변은 하나의 질문에 여러개가 달릴 수 있다.
+	 * 답변은 Many가 되고 질문은 하나가 된다.
+	 * ManyToOne (N:1 관계)
+	 * 
+	 * 
+	 * */
+	
 
 }
