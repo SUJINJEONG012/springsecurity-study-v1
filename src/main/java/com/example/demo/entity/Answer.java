1d653eff51347367c9f623411c3b11911fcd8f0c
SUJINJEONG012 <peekaboo32@naver.com>
2023-06-18 05:02:35 +0900

    gradle에 mysql 설정
    com.mysql:mysql-connector-j 로 변경하니 오류사라짐

--- src/main/java/com/example/demo/entity/Answer.java
+++ src/main/java/com/example/demo/entity/Answer.java
@@ -2,12 +2,11 @@ package com.example.demo.entity;
 
 import java.time.LocalDateTime;
 
-import org.springframework.data.annotation.Id;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import lombok.Data;
 
--- src/main/java/com/example/demo/entity/Question.java
+++ src/main/java/com/example/demo/entity/Question.java
@@ -3,13 +3,12 @@ package com.example.demo.entity;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import org.springframework.data.annotation.Id;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import lombok.Data;
 
