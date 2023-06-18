7f26566c1fea8b4e34375cf403c5b6f1e2649f90
SUJINJEONG012 <peekaboo32@naver.com>
2023-06-18 23:35:51 +0900

    컨트롤러에서 맵핑부분  설정했다. 저장은 서비스에서 메서드 생성하자.

--- src/main/java/com/example/demo/controller/QuestionController.java
+++ src/main/java/com/example/demo/controller/QuestionController.java
@@ -6,7 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.entity.Question;
 import com.example.demo.service.QuestionService;
@@ -34,8 +36,15 @@ public class QuestionController {
 		return "question_detail";
 	}
 	
+	/* 리스트 페이지에서 버튼클릭시 질문등록 페이지로 이동 */
 	@GetMapping("/create")
 	public String questionCreate() {
 		return "question_form";
+	}
+	
+	/* 질문등록 버튼 클릭시 post형식으로 보내기*/
+	@PostMapping("/create")
+	public String questionCreate(@RequestParam String subject, @RequestParam String content) {
+		return "redirect:/question/list"; //질문 저장후 질문목록으로 이동
 	}
 }
