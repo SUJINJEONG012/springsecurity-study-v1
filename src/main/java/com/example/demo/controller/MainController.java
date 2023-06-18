fa70176ec7c0c9f674dafd9e040b57a973822bfd
SUJINJEONG012 <peekaboo32@naver.com>
2023-06-18 17:26:59 +0900

    질문리스트 뷰에 뿌리기

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,15 +1,12 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MainController {
 	@GetMapping("/")
-	
 	public String index() {
-		return "안녕하세요. 스프링부트 시큐리티 공부를 위한 프로젝트";
-				
+		return "redirect:/question/list";		
 	}
 }
--- src/main/java/com/example/demo/controller/QuestionController.java
+++ src/main/java/com/example/demo/controller/QuestionController.java
@@ -1,14 +1,26 @@
 package com.example.demo.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.List;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.entity.Question;
+import com.example.demo.repository.QuestionRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Controller
 public class QuestionController {
 
+	private final QuestionRepository questionRepository;
+	
 	@GetMapping("/question/list")
-	public String list() {
+	public String list(Model model) {
+		List<Question> questionList = this.questionRepository.findAll();
+		model.addAttribute("questionList", questionList);
 		return "question_list";
 	}
 }
