f7539a3932edc543590bdbef47779576a150d6f1
SUJINJEONG012 <peekaboo32@naver.com>
2023-06-20 00:04:13 +0900

    질문하기 등록 후 데이터베이스 저장

--- src/main/java/com/example/demo/controller/QuestionController.java
+++ src/main/java/com/example/demo/controller/QuestionController.java
@@ -4,15 +4,17 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.entity.Question;
+import com.example.demo.entity.QuestionForm;
 import com.example.demo.service.QuestionService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RequestMapping("/question")
@@ -21,6 +23,7 @@ import lombok.RequiredArgsConstructor;
 public class QuestionController {
 
 	private final QuestionService questionService;
+	
 	
 	@GetMapping("/list")
 	public String list(Model model) {
@@ -38,13 +41,18 @@ public class QuestionController {
 	
 	/* 리스트 페이지에서 버튼클릭시 질문등록 페이지로 이동 */
 	@GetMapping("/create")
-	public String questionCreate() {
+	public String questionCreate(QuestionForm questionForm) {
 		return "question_form";
 	}
 	
 	/* 질문등록 버튼 클릭시 post형식으로 보내기*/
 	@PostMapping("/create")
-	public String questionCreate(@RequestParam String subject, @RequestParam String content) {
+	public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult) {
+		if(bindingResult.hasErrors()) {
+			return "question_form";
+		}
+		this.questionService.create(questionForm.getSubject(), questionForm.getContent());
 		return "redirect:/question/list"; //질문 저장후 질문목록으로 이동
 	}
+
 }
--- src/main/java/com/example/demo/service/QuestionService.java
+++ src/main/java/com/example/demo/service/QuestionService.java
@@ -1,11 +1,13 @@
 package com.example.demo.service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.Question;
+import com.example.demo.entity.QuestionForm;
 import com.example.demo.repository.QuestionRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -15,18 +17,27 @@ import lombok.RequiredArgsConstructor;
 public class QuestionService {
 
 	private final QuestionRepository questionRepository;
-	
-	public List<Question> getList(){
+
+	public List<Question> getList() {
 		return this.questionRepository.findAll();
 	}
-	
-	
+
+	/* 질문 가져오기 */
 	public Question getQuestion(Integer id) {
 		Optional<Question> question = this.questionRepository.findById(id);
-		if(question.isPresent()) {
+		if (question.isPresent()) {
 			return question.get();
-		}else {
+		} else {
 			throw new DataNotFoundException("!!!! question not found !!!!!");
 		}
 	}
+
+	public void create(String subject, String content) {
+		Question q = new Question();
+		q.setSubject(subject);
+		q.setContent(content);
+		q.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q);
+	}
+
 }
