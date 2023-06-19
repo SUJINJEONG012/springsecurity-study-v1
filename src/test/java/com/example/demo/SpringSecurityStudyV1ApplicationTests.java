8e6bc901cc807af5f9dfb4e998e9aca582cff038
angela <peekaboo32@naver.com>
2023-06-19 14:45:31 +0900

    테스트 코드 디비에 들어가는 값 오류 확인

--- src/test/java/com/example/demo/SpringSecurityStudyV1ApplicationTests.java
+++ src/test/java/com/example/demo/SpringSecurityStudyV1ApplicationTests.java
@@ -23,26 +23,26 @@ class SpringSecurityStudyV1ApplicationTests {
 	@Autowired
 	private QuestionRepository questionRepository;
 	
-	@Autowired
-	private AnswerRepository answerRepository;
+//	@Autowired
+//	private AnswerRepository answerRepository;
 	
 	
 	
-	@Transactional
+	
 	@Test
 	void contextLoads() {
 		//데이터생성
-//		Question q1 = new Question();
-//		q1.setSubject("스프링부트가뭔가요 ?");
-//		q1.setContent("스프링 부트와 스프링의차이점을 차이점을 알기 위해 학습중입니다.");
-//		q1.setCreateDate(LocalDateTime.now());
-//		this.questionRepository.save(q1);
-//		
-//		Question q2 = new Question();
-//		q2.setSubject("개발이 재밌지만 잘 할 수 있을까요?");
-//		q2.setContent("저는 스프링부트를 잘다룰수 있을까요 ?");
-//		q2.setCreateDate(LocalDateTime.now());
-//		this.questionRepository.save(q2);
+		Question q1 = new Question();
+		q1.setSubject("스프링부트가뭔가요 ?");
+		q1.setContent("스프링 부트와 스프링의차이점을 차이점을 알기 위해 학습중입니다.");
+		q1.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q1);
+		
+		Question q2 = new Question();
+		q2.setSubject("개발이 재밌지만 잘 할 수 있을까요?");
+		q2.setContent("저는 스프링부트를 잘다룰수 있을까요 ?");
+		q2.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q2);
 		
 		//데이터 조회
 //		List<Question> all = this.questionRepository.findAll();
@@ -102,14 +102,14 @@ class SpringSecurityStudyV1ApplicationTests {
 //		System.out.println(a.getQuestion().getId());
 	
 		
-		Optional<Question> oq = this.questionRepository.findById(2);
-        assertTrue(oq.isPresent());
-        Question q = oq.get();
-
-        List<Answer> answerList = q.getAnswerList();
-
-        assertEquals(1, answerList.size());
-        assertEquals("답변 생성 테스트 ", answerList.get(0).getContent());
+//		Optional<Question> oq = this.questionRepository.findById(2);
+//        assertTrue(oq.isPresent());
+//        Question q = oq.get();
+//
+//        List<Answer> answerList = q.getAnswerList();
+//
+//        assertEquals(1, answerList.size());
+//        assertEquals("답변 생성 테스트 ", answerList.get(0).getContent());
 	}
 
 }
