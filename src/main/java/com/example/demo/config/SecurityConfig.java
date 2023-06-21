ef9657fe74eabfa2e8b9a786e01579acea0ef741
SUJINJEONG012 <peekaboo32@naver.com>
2023-06-21 22:54:36 +0900

    시큐리티 초기설정

---
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+/*
+ * @Configuration은 스프링의 환경설정 파일
+ * @EnableWebSecurity는 모든 요청 URL이 스프링 시큐리티의 제어를 받도록 만드는 애너테이션
+ * */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	
+	@Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests().requestMatchers(
+                new AntPathRequestMatcher("/**")).permitAll()
+                ;
+        return http.build();
+    }
+	
+}
