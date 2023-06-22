124f700ea02b39337728b5a58013326822edf11a
angela <peekaboo32@naver.com>
2023-06-22 17:58:35 +0900

    SecurityConfig에  패스워드인코더 빈 등록

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -18,6 +20,7 @@ public class SecurityConfig {
 	private static final String[] AUTH_WHITELIST = {
             "/", "/question/**"
     };
+	
 	@Bean
     protected SecurityFilterChain config(HttpSecurity http) throws Exception {
         return http
@@ -28,5 +31,10 @@ public class SecurityConfig {
                         .authenticated())
                 .build();
     }
+	
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 	
 }
