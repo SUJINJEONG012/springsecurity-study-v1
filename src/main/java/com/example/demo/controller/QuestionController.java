b6b0148307d3dc1e60e8c461fca8b605b81fbc6b
SUJINJEONG012 <peekaboo32@naver.com>
2023-06-25 18:27:39 +0900

    질문수정/삭제 구현완료  ! !

--- src/main/java/com/example/demo/controller/QuestionController.java
+++ src/main/java/com/example/demo/controller/QuestionController.java
@@ -111,6 +111,17 @@ public class QuestionController {
 		
 	}
 	
+	@PreAuthorize("isAuthenticated()")
+	@GetMapping("/delete/{id}")
+	public String delete(Principal principal, @PathVariable("id") Integer id) {
+		Question question = this.questionService.getQuestion(id);
+		if(!question.getAuthor().getUsername().equals(principal.getName())) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제권한이 없습니다.");
+		}
+		this.questionService.delete(question);
+		return "redirect:/";
+	}
+	
 	
 	
 
