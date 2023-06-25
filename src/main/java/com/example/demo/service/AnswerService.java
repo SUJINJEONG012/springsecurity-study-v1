949d8ed331093fd6973e95e5622b2d1e083ca36e
SUJINJEONG012 <peekaboo32@naver.com>
2023-06-25 18:41:45 +0900

    답변부분도 수정삭제기능 추가  > answer_form 파일 생성

--- src/main/java/com/example/demo/service/AnswerService.java
+++ src/main/java/com/example/demo/service/AnswerService.java
@@ -1,9 +1,11 @@
 package com.example.demo.service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.example.demo.config.DataNotFoundException;
 import com.example.demo.entity.Answer;
 import com.example.demo.entity.Question;
 import com.example.demo.entity.SiteUser;
@@ -25,4 +27,21 @@ public class AnswerService {
 		answer.setAuthor(author);
 		this.answerRepository.save(answer);
 	}
+	
+	public Answer getAnswer(Integer id) {
+		Optional<Answer> answer = this.answerRepository.findById(id);
+		
+		if(answer.isPresent()) {
+			return answer.get();
+		}else {
+			throw new DataNotFoundException("answer not found !!!");
+		}
+	}
+	
+	public void modify(Answer answer, String content) {
+		answer.setContent(content);
+		answer.setModifyDate(LocalDateTime.now());
+		this.answerRepository.save(answer);
+	}
+	
 }
